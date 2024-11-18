1f8e69d970eaf85c299e5f0d187d1625d9d065e8
Shihab Mahamud <shihab4t@gmail.com>
2024-11-18 23:06:07 +0600

    prettier setup

--- 02_rails-inertia-react/vite.config.ts
+++ 02_rails-inertia-react/vite.config.ts
@@ -1,10 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import RubyPlugin from 'vite-plugin-ruby';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    RubyPlugin(),
-  ],
-})
+    plugins: [react(), RubyPlugin()],
+});
